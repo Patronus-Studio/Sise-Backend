7abc6a47e7e07141a88c976e044b96f30da5efba
Suleyman SEZER <me.iamcodder@gmail.com>
2022-08-07 23:04:44 +0300

    Test file removed

--- src/test/kotlin/com/patronusstudio/BottleFlip/BottleFlipApplicationTests.kt
+++ src/test/kotlin/com/patronusstudio/BottleFlip/BottleFlipApplicationTests.kt
@@ -1,13 +1,1 @@
 package com.patronusstudio.BottleFlip
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class BottleFlipApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
-}
