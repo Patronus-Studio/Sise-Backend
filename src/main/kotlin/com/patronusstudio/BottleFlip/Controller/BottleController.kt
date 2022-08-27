ffc305568f87a176593f1ff11937a8f27b3425e9
Suleyman SEZER <me.iamcodder@gmail.com>
2022-08-27 16:00:59 +0300

    Get bottle service added

--- src/main/kotlin/com/patronusstudio/BottleFlip/Controller/BottleController.kt
+++ src/main/kotlin/com/patronusstudio/BottleFlip/Controller/BottleController.kt
@@ -4,10 +4,7 @@ import com.patronusstudio.BottleFlip.Base.BaseResponse
 import com.patronusstudio.BottleFlip.Model.BottleModel
 import com.patronusstudio.BottleFlip.Service.BottleService
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/bottle")
 @RestController
@@ -19,5 +16,25 @@ class BottleController {
     @PostMapping("/insertBottle")
     fun insertBottle(@RequestBody model: BottleModel): BaseResponse {
         return bottleService.insertBottle(model)
+    }
+
+    @PostMapping("/getBottleByUsername")
+    fun getBottleByUsername(@RequestParam username:String):BaseResponse{
+        return bottleService.getBottleByUsername(username)
+    }
+
+    @PostMapping("/getBottleByBottleName")
+    fun getBottleByBottleName(@RequestParam bottleName:String):BaseResponse{
+        return bottleService.getBottleByBottleName(bottleName)
+    }
+
+    @PostMapping("/getPackageByMostLike")
+    fun getBottleByMostLike():BaseResponse{
+        return bottleService.getBottleFromMostLike()
+    }
+
+    @PostMapping("/getPackageByMostDownload")
+    fun getBottleByMostDownload():BaseResponse{
+        return bottleService.getBottleFromMostDownload()
     }
 }
--- src/main/kotlin/com/patronusstudio/BottleFlip/Service/BottleService.kt
+++ src/main/kotlin/com/patronusstudio/BottleFlip/Service/BottleService.kt
@@ -4,6 +4,7 @@ import com.patronusstudio.BottleFlip.Base.BaseResponse
 import com.patronusstudio.BottleFlip.Base.BaseSealed
 import com.patronusstudio.BottleFlip.Model.BottleModel
 import com.patronusstudio.BottleFlip.Model.ErrorResponse
+import com.patronusstudio.BottleFlip.Model.PackageModel
 import com.patronusstudio.BottleFlip.Model.SuccesResponse
 import com.patronusstudio.BottleFlip.Repository.SqlRepo
 import com.patronusstudio.BottleFlip.enums.CreateTableSqlEnum
@@ -17,13 +18,13 @@ class BottleService {
     @Autowired
     private lateinit var sqlRepo: SqlRepo
 
-    fun insertBottle(model:BottleModel): BaseResponse {
+    fun insertBottle(model: BottleModel): BaseResponse {
         val sql = CreateTableSqlEnum.BOTTLES.getCreateSql()
         val bottleTable = sqlRepo.setData(sql)
         return if (bottleTable is BaseSealed.Succes) {
             val insertSql = CreateTableSqlEnum.BOTTLES.getDefaultInsertSql(
-                model.username ?: "",model.name ?: "",model.description ?: "",model.luckRatio.toString(),
-                model.bottleType.toString(),model.imageUrl ?: ""
+                model.username ?: "", model.name ?: "", model.description ?: "", model.luckRatio.toString(),
+                model.bottleType.toString(), model.imageUrl ?: ""
             )
             val insertResult = sqlRepo.setData(insertSql)
             if (insertResult is BaseSealed.Succes) {
@@ -34,6 +35,48 @@ class BottleService {
             }
         } else {
             ErrorResponse("Db oluşturulurken bir hata oluştur", HttpStatus.NOT_ACCEPTABLE)
+        }
+    }
+
+    fun getBottleByUsername(username: String): BaseResponse {
+        val sql = "SELECT * FROM bottles where username = \"$username\""
+        val userBottles = sqlRepo.getDataForList(sql, BottleModel())
+        return if (userBottles is BaseSealed.Succes) {
+            SuccesResponse(data = userBottles.data, status = HttpStatus.OK, message = null)
+        } else {
+            userBottles as BaseSealed.Error
+            ErrorResponse("Bu kullanıcının oluşturduğu şişe bulunamadı", HttpStatus.NOT_ACCEPTABLE)
+        }
+    }
+
+    fun getBottleByBottleName(bottleName: String): BaseResponse {
+        val sql = "SELECT * FROM bottles where name = \"$bottleName\""
+        val userBottles = sqlRepo.getDataForList(sql, BottleModel())
+        return if (userBottles is BaseSealed.Succes) {
+            SuccesResponse(data = userBottles.data, status = HttpStatus.OK, message = null)
+        } else {
+            userBottles as BaseSealed.Error
+            ErrorResponse("Şişe bulunamadı", HttpStatus.NOT_ACCEPTABLE)
+        }
+    }
+
+    fun getBottleFromMostLike():BaseResponse{
+        val sql = "Select * from bottles order by numberOfLike desc"
+        val result = sqlRepo.getDataForList(sql, PackageModel())
+        return if (result is BaseSealed.Succes) {
+            SuccesResponse(data = result.data, status = HttpStatus.OK, message = null)
+        } else {
+            ErrorResponse("Paket bulunamadı", HttpStatus.NOT_ACCEPTABLE)
+        }
+    }
+
+    fun getBottleFromMostDownload():BaseResponse{
+        val sql = "Select * from bottles order by numberOfDownload desc"
+        val result = sqlRepo.getDataForList(sql, PackageModel())
+        return if (result is BaseSealed.Succes) {
+            SuccesResponse(data = result.data, status = HttpStatus.OK, message = null)
+        } else {
+            ErrorResponse("Paket bulunamadı", HttpStatus.NOT_ACCEPTABLE)
         }
     }
 }
