8c9132dfe20d35d94245635f1b49e4a0b7d2e433
Süleyman SEZER <hi@iamcodder.com>
2022-10-30 17:52:37 +0300

    Token property name changed to message

--- src/main/kotlin/com/patronusstudio/BottleFlip/Model/SuccesResponse.kt
+++ src/main/kotlin/com/patronusstudio/BottleFlip/Model/SuccesResponse.kt
@@ -1,11 +1,10 @@
 package com.patronusstudio.BottleFlip.Model
 
-import com.fasterxml.jackson.annotation.JsonProperty
 import com.patronusstudio.BottleFlip.Base.BaseResponse
 import org.springframework.http.HttpStatus
 
 data class SuccesResponse(
-    @JsonProperty("token") var message: String? = null,
+    var message: String? = null,
     override val status: HttpStatus,
     var data: Any? = null
 ) : BaseResponse()
